6a72dd432599e440c6d070f2a29198f9aef65830
Nimish20 <ngupta@gammastack.com>
2020-03-04 10:50:49 +0530

    correct migration

--- db/migrate/20200227111903_add_reference_to_orders.rb
+++ db/migrate/20200227111903_add_reference_to_orders.rb
@@ -1,5 +1,5 @@
-class AddReferenceToStores < ActiveRecord::Migration[6.0]
+class AddReferenceToOrders < ActiveRecord::Migration[6.0]
   def change
-    add_reference :stores, :seller, foreign_key: true
+    add_reference :orders, :cart, foreign_key: true
   end
 end
