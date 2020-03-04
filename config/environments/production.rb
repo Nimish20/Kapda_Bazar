07b09dadc5a853d236353414ea8dc4cf818b5135
Nimish20 <ngupta@gammastack.com>
2020-03-04 11:53:41 +0530

    update mailing configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'nimishgupta35@gmail.com',
-    password: ENV['EMAIL_PASSWORD'],
+    password: 'password1997',
     authentication: 'plain',
     enable_starttls_auto: true
   }
